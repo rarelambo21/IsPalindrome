3f0e540bce07d51824b4d7d5c1a7314182eadef6
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-02 11:22:19 +0300

    isPalindrome

--- IsPalindrome/IsPalindrome.cpp
+++ IsPalindrome/IsPalindrome.cpp
@@ -2,19 +2,36 @@
 //
 
 #include <iostream>
+#include <string>
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string str, revStr = "";
+
+
+    std::cout << "Enter a single word: !\n";
+    std::cin >> str;
+
+    int len = (int)str.length();
+
+    revStr.resize(len);
+
+    for (int i = 0, j = len - 1; i < len; i++, j--) {
+        revStr[i] = str[j];
+    }
+    revStr[len] = '\0';
+
+    std::cout << str << " " << revStr << std::endl;
+
+    if (str == revStr)
+        std::cout << "is Palindrome";
+
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
